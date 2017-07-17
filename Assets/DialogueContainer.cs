2be4ad98f95150ed4ae72d17021e7390d8206dc0
Sennek <dronsennek@gmail.com>
2017-07-17 16:41:09 +0300

    Add files via upload

---
+++ Assets/DialogueContainer.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Serialization;
+using System;
+
+namespace DialogueContainer
+{
+    
+    [Serializable, XmlRoot("Dialogue")]
+    public class Dialogue
+    {
+        [XmlElement("Node")]
+        public Node[] nodes;
+    }
+
+    [Serializable]
+    public class Node
+    {
+        [XmlAttribute("Id")]
+        public int id;
+        [XmlElement("Text")]
+        public string text;
+        [XmlElement("Option")]
+        public Option[] options;
+    }
+    
+    [Serializable]
+    public class Option
+    {
+        [XmlAttribute("ToNodeId")]
+        public int id;
+        [XmlText]
+        public string text;
+    }
+}
---
+++ Assets/xmltest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Xml.Serialization;
+using DialogueContainer;
+
+public class xmltest : MonoBehaviour
+{
+    public Dialogue dia;
+
+    public void Start()
+    {
+        string path = Path.Combine(Application.dataPath, "test.xml");
+        XmlSerializer serializer = new XmlSerializer(typeof(Dialogue));
+        using (var stream = new FileStream(path, FileMode.Open))
+        {
+            dia = (Dialogue)serializer.Deserialize(stream);
+        }
+    }
+
+    public void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(0, 0, 500, 500));
+        GUILayout.BeginVertical();
+        if (GUILayout.Button("save", GUILayout.Width(100), GUILayout.Height(50)))
+        {
+            
+        }
+        if (GUILayout.Button("Print", GUILayout.Width(100), GUILayout.Height(50)))
+        {
+            
+        }
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
+
+    }
+
+}
